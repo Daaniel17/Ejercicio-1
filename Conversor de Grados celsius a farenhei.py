2ff0488541bdf071e4e94332b309805015b53e16
Daaniel17 <117611044+Daaniel17@users.noreply.github.com>
2022-11-07 14:26:08 -0500

    Add files via upload

---
+++ Conversor de Grados celsius a farenhei.py
@@ -0,0 +1,12 @@
+#
+
+import math
+
+def formula():
+    farenheit= celsius*9 /5+32
+def principal():
+    celsius=int(input("Ingrse los grados celcius que quiere convertir a farenheit: "))
+    farenheit= celsius*9 /5+32
+    print(celsius," grados Celsius son: ",farenheit," grados Fahrenheit.")
+
+principal()
